fc1fff418d97c30533df320fba704cccfd59ad6b
Matt Hart <mahart2941@gmail.com>
2022-05-17 03:46:39 -0400

    adds todos

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ CS = os.environ.get('SPOTIFY_CLIENT_SECRET')
 
 # Assumes you followed: https://ytmusicapi.readthedocs.io/en/latest/setup.html
 yt = YTMusic('headers_auth.json')
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=CID,client_secret=CS))
+manager = SpotifyClientCredentials(client_id=CID,client_secret=CS)
+sp = spotipy.Spotify(auth_manager=manager)
 
 # logging
 logging.basicConfig(filename='info.log', filemode='w', format='%(levelname)s - %(message)s', level=logging.INFO)
@@ -26,15 +27,17 @@ logging.basicConfig(filename='info.log', filemode='w', format='%(levelname)s - %
 def main():
     ids = []
     spotify_tracks = get_spotify_tracks(sp, SPOTIFY_PLAYLIST_ID)
-    print(len(spotify_tracks))
+
     for track in spotify_tracks:
         song_search_string = get_song_search_string(track)
-        # the limit parameter of search does not seem to work so slice the response
+        # TODO determine if and when rate limiting occurs for hitting the search endpoint. Handle as needed.
+        # the limit parameter of the search function does not seem to work so slice the response
         song_results = yt.search(query=song_search_string, filter='songs')[0:3]
         video_id = parse_vid(song_results)
         if video_id:
             ids.append(video_id)
     logging.info(f'Found: {len(ids)} videoIds. Attempting to create playlist')
+    # TODO determine what the max number of video_ids you can send to create/playlist endpoint at once.
     response = yt.create_playlist(title=YOUTUBE_PLAYLIST_NAME,description=YOUTUBE_PLAYLIST_DESCRIPTION,video_ids=ids)
     logging.info(response)
 
