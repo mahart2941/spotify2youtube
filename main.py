e650168b33ad885e0b11866a57cfe2bb9926ccf7
Matt Hart <mahart2941@gmail.com>
2022-05-17 04:10:48 -0400

    god I hate Flake8

--- main.py
+++ main.py
@@ -3,25 +3,29 @@ import spotipy
 import logging
 from spotipy.oauth2 import SpotifyClientCredentials
 from ytmusicapi import YTMusic
-from typing import List
-from utils import * # noqa
+from utils import get_spotify_tracks, get_song_search_string, parse_vid
 
 # Fill in as needed
 SPOTIFY_PLAYLIST_ID = 'SPID'
 YOUTUBE_PLAYLIST_NAME = 'NAME'
 YOUTUBE_PLAYLIST_DESCRIPTION = 'DESC'
 
-# Assumes you've created environmental variables from https://developer.spotify.com/dashboard/
+# Assumes you've created environmental variables from
+# https://developer.spotify.com/dashboard/
 CID = os.environ.get('SPOTIFY_CLIENT_ID')
 CS = os.environ.get('SPOTIFY_CLIENT_SECRET')
 
 # Assumes you followed: https://ytmusicapi.readthedocs.io/en/latest/setup.html
 yt = YTMusic('headers_auth.json')
-manager = SpotifyClientCredentials(client_id=CID,client_secret=CS)
+manager = SpotifyClientCredentials(client_id=CID, client_secret=CS)
 sp = spotipy.Spotify(auth_manager=manager)
 
 # logging
-logging.basicConfig(filename='info.log', filemode='w', format='%(levelname)s - %(message)s', level=logging.INFO)
+logging.basicConfig(
+    filename='info.log', filemode='w',
+    format='%(levelname)s - %(message)s',
+    level=logging.INFO
+)
 
 
 def main():
@@ -30,16 +34,24 @@ def main():
 
     for track in spotify_tracks:
         song_search_string = get_song_search_string(track)
-        # TODO determine if and when rate limiting occurs for hitting the search endpoint. Handle as needed.
-        # the limit parameter of the search function does not seem to work so slice the response
+        # TODO determine if and when rate limiting occurs for
+        # hitting the search endpoint. Handle as needed.
+        # the limit parameter of the search function does not seem to work so
+        # slice the response
         song_results = yt.search(query=song_search_string, filter='songs')[0:3]
         video_id = parse_vid(song_results)
         if video_id:
             ids.append(video_id)
     logging.info(f'Found: {len(ids)} videoIds. Attempting to create playlist')
-    # TODO determine what the max number of video_ids you can send to create/playlist endpoint at once.
-    response = yt.create_playlist(title=YOUTUBE_PLAYLIST_NAME,description=YOUTUBE_PLAYLIST_DESCRIPTION,video_ids=ids)
+    # TODO determine what the max number of video_ids you can send to
+    # create/playlist endpoint at once.
+    response = yt.create_playlist(
+        title=YOUTUBE_PLAYLIST_NAME,
+        description=YOUTUBE_PLAYLIST_DESCRIPTION,
+        video_ids=ids
+    )
     logging.info(response)
+
 
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -5,23 +5,34 @@ from typing import List, Optional
 
 log = logging.getLogger(__name__)
 
-# TODO: this is super simple but works. could be improved by comparing attributes of the spotify track and youtube song (name, artists, seconds duration, explicit bool, release/upload date)
-# Sigma does this here: https://github.com/sigma67/spotifyplaylist_to_ytmusic/blob/b08f3b5b0b6b2d0f3d87686fb9a1e6a848c92684/YouTube.py#L21
+'''
+TODO: get_song_search_string() is super simple but works. could be improved by
+comparing attributes of the spotify track and youtube song (name, artists,
+seconds duration, explicit bool, release/upload date)
+Sigma does this here:
+https://github.com/sigma67/spotifyplaylist_to_ytmusic/blob/b08f3b5b0b6b2d0f3d87686fb9a1e6a848c92684/YouTube.py#L21
+'''
+
+
 def get_song_search_string(song_dict: dict) -> str:
-    """Parses a Spotify API song dictionary and returns a string joining the name and all artists
+    """Parses a Spotify API song dictionary and returns a string joining the
+    name and all artists
 
     Example: Return to Oz by Monolink, ARTBAT
 
     Args:
-        song_dict (dict): the `track` dictionary that the Spotify API returns. The default returned fields should contain name and artists
+        song_dict (dict): the `track` dictionary that the Spotify API returns.
+        The default returned fields should contain name and artists
 
     Returns:
-        str: a string 
+        str: the song search string
     """
 
     '''
-    Split the name on `(` to get the song base name removing extra info. 
-    Example: Mother Nature (feat. NOISY). We don't need to know featured artists as it will be in the artists dict. May cause issues for more niche songs.
+    Split the name on `(` to get the song base name removing extra info.
+    Example: Mother Nature (feat. NOISY). We don't need to know featured
+    artists as it will be in the artists dict. May cause issues for more
+    niche songs.
     '''
     song_name = song_dict['name'].split(' (')[0]
     song_artists = ', '.join([artist['name'] for artist in song_dict['artists']])
@@ -46,25 +57,34 @@ def parse_vid(songs: list) -> Optional[str]:
 
         return None
 
-def get_spotify_tracks(spotify_client: spotipy.Spotify, spotify_id: str) -> List[dict]:
-    """Get 
 
+def get_spotify_tracks(spotify_client: spotipy.Spotify, spotify_id: str) -> List[dict]:
+    """Get all tracks from the given spotify playlist id
     Args:
         spotify_client (spotipy.Spotify): Spotify Client object
         spotify_id (str): The spotify guid for the playlis
 
     Returns:
-        List[dict]: A list of track dictionaries which will include track name and track artists
+        List[dict]: A list of track dictionaries which will include
+        track name and track artists
     """
-    # create a range for the number of requests it will take to get total tracks in a playlist. max requested tracks at a time is 100
-    iteration_range = range(int(spotify_client.playlist_items(spotify_id,fields='total')['total']/100) + 1)
+    '''create a range for the number of requests it will take to get total
+    tracks in a playlist. max requested tracks at a time is 100'''
+    total_songs = spotify_client.playlist_items(spotify_id, fields='total')['total']
+    iteration_range = range(int(total_songs)/100 + 1)
     playlist_tracks = []
 
     offset = 0
     for i in iteration_range:
-        # fields to expand on later: items.track.name, items.track.artists.name, items.track.duration_ms, items.track.release_date, items.track.explicit
-        current_tracks = spotify_client.playlist_items(spotify_id, fields='items.track.name, items.track.artists.name', offset=offset)['items']
-        # "flatten" results so that each dict in the playlist tracks list just contains a dict with the keys: name, artists 
+        ''' fields to expand on later: items.track.name,
+        items.track.artists.name, items.track.duration_ms,
+        items.track.release_date, items.track.explicit'''
+        current_tracks = spotify_client.playlist_items(
+            spotify_id, fields='items.track.name, items.track.artists.name',
+            offset=offset
+        )['items']
+        '''"flatten" results so that each dict in the playlist tracks list
+        just contains a dict with the keys: name, artists'''
         playlist_tracks.extend([track['track'] for track in current_tracks])
         offset += 100
     logging.info(f'We found a total of: {len(playlist_tracks)} songs')
